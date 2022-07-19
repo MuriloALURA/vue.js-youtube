15bab028af66dd4d2769d3662bbafda37d44d4e9
Murilo <murilo.alvess@yahoo.com>
2022-07-19 11:56:53 -0300

    feat: 🛠 axios, rota, api, bootstrap

---
+++ vue-cli/my-project/src/components/GitHubIssues.vue
@@ -0,0 +1,90 @@
+<template>
+<div class="container">
+    <h1>Vue.js + Github</h1>
+    <p class="lead">
+        Página que lista issues de um repositório do Github, usando Vue.js.
+    </p>
+
+    <div class="row">
+        <div class="col">
+            <div class="form-group">
+                <input v-model="username" type="text" class="form-control" placeholder="github username">
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="form-group">
+                <input v-model="repository" type="text" class="form-control" placeholder="github repositório">
+            </div>
+        </div>
+
+        <div class="col-3">
+            <div class="form-group">
+                <button @click.stop.prevent="getIssues()" class="btn btn-success">GO</button>
+
+                <button @click.stop.prevent="reset()" class="btn btn-danger">LIMPAR</button>
+            </div>
+        </div>
+    </div>
+
+    <br>
+    <hr><br>
+
+    <table class="table table-sm table-bordered">
+        <thead>
+            <tr>
+                <th width="100">Número</th>
+                <th>Título</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr v-if="!!issues.length"
+            v-for="issue in issues" 
+            :key="issue.number">
+                <td>{{issue.number}}</td>
+                <td>{{issue.title}}</td>             
+            </tr>
+
+            <tr v-if="!!!issues.length">
+                <td class="text-center" colspan="2">Nenhuma issues encontrada!</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'GitHubIssues',
+
+    data() {
+        return {
+            username: '',
+            repository: '',
+            issues: [],
+        };
+    },
+
+    methods: {
+        reset() {
+            this.username = '';
+            this.repository = '';
+        },
+        getIssues() {
+            if (this.username && this.repository) {
+
+                // eslint-disable-next-line
+                const url = `https://api.github.com/repos/${this.username}/${this.repository}/issues`;
+                axios.get(url).then((response) => {
+                this.issues = response.data;                    
+                    // eslint-disable-next-line
+                    console.log(response)
+                });
+            }
+        }
+    },
+};
+</script>
